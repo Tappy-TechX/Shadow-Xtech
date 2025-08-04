754d52f5f5713eee481bd53ac82c656cc91b8c89
Tappy-TechX <tappyblack2@gmail.com>
2025-08-04 19:52:55 +0300

    ֆཏɑɖօա-𝕏Ե𝖾𝖼ཏ

--- plugins/ping-status.js
+++ plugins/ping-status.js
@@ -12,7 +12,7 @@ const quotedContact = {
   },
   message: {
     contactMessage: {
-      displayName: "⚙️ Latency-Check | Verified ✅",
+      displayName: "⚙️ Latency | Check 🚀",
       vcard: "BEGIN:VCARD\nVERSION:3.0\nFN:SCIFI\nORG:Shadow-Xtech BOT;\nTEL;type=CELL;type=VOICE;waid=254700000001:+254 700 000001\nEND:VCARD"
     }
   }
