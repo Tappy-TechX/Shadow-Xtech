8e1d0789285bd95a0bea797ba33271dc79c4e1eb
Tappy-TechX <tappyblack2@gmail.com>
2026-04-04 13:31:49 +0300

    Sʜᴀᴅᴏᴡ-Xᴛᴇᴄʜ

--- plugins/download-modified.js
+++ plugins/download-modified.js
@@ -1,7 +1,4 @@
 const { fetchJson } = require("../lib/functions");
-const { downloadTiktok } = require("@mrnima/tiktok-downloader");
-const { facebook } = require("@mrnima/facebook-downloader");
-const cheerio = require("cheerio");
 const { igdl } = require("ruhend-scraper");
 const axios = require("axios");
 const { cmd, commands } = require('../command');
